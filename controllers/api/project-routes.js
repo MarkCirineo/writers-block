18bac844d8d52639236d2e9a5ddcd8d855275e18
lamperouge <philbohn1791@gmail.com>
2021-10-20 14:33:58 -0400

    delete route for project-routes.js completed, theoretically

--- controllers/api/project-routes.js
+++ controllers/api/project-routes.js
@@ -1,6 +1,7 @@
 // Individual pages are for getting the specific data from the database
 // Full CRUD operations
 const router = require("express").Router();
+const { route } = require(".");
 const { User, Project, Topic_Sentence, Works_Cited } = require("../../models");
 
 //TODO: GET a list of all projects
@@ -31,6 +32,9 @@ router.get("/:id", async (req, res) => {
         },
       ],
     });
+    if (!projectData) {
+      res.status(404).json({ message: "No project with this ID" });
+    }
     res.status(200).json(projectData);
   } catch (err) {
     res.status(500).json(err);
@@ -55,5 +59,20 @@ router.post("/", async (req, res) => {
 //TODO: UPDATE a project
 
 //TODO: DELETE a project
+router.delete("/:id", async (req, res) => {
+  try {
+    const projectData = await Project.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!projectData) {
+      res.status(404).json({ message: "No project with this ID" });
+    }
+    res.status(200).json({ message: "This project has been deleted!" });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
