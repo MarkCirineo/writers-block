ff047db505ee210cf5aa84cccd175eba65e72916
lamperouge <philbohn1791@gmail.com>
2021-10-20 14:23:00 -0400

    both GET routes for Projects in project-routes.js, seemingly, complete

--- controllers/api/project-routes.js
+++ controllers/api/project-routes.js
@@ -4,8 +4,38 @@ const router = require("express").Router();
 const { User, Project, Topic_Sentence, Works_Cited } = require("../../models");
 
 //TODO: GET a list of all projects
+router.get("/", async (req, res) => {
+  try {
+    const projectData = await Project.findAll();
+    res.status(200).json(projectData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 //TODO: GET a project by ID
+router.get("/:id", async (req, res) => {
+  try {
+    const projectData = await Project.findOne({
+      where: {
+        id: req.params.id,
+      },
+      include: [
+        {
+          model: Topic_Sentence,
+          attributes: ["sentence", "project_id"],
+        },
+        {
+          model: Works_Cited,
+          attributes: ["content", "project_id"],
+        },
+      ],
+    });
+    res.status(200).json(projectData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 //TODO: CREATE a new project
 
