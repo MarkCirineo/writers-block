28ee1c4a06659e3ebb68714d3331066e88a5b21f
isaac long <isaaclong123@outlook.com>
2021-10-20 14:34:52 -0400

    worked on html

---
+++ public/js/dashboard.js
@@ -0,0 +1,35 @@
+// $(document).ready(function(){
+
+
+// var projects = $("#projectGrid").children("div")
+
+// var counts = projects.length;
+
+// const newRow = `</div> <div class="row">`
+
+// const firstRow = `<div class="row">`
+// projects.each(function(){
+   
+//     var id= $(this).attr("id") 
+//     if(id == 1){
+//         $(this).before(firstRow)
+//         console.log(firstRow)
+//     }
+
+//     if( id % 3 === 0 ){
+//         $(this).after(newRow)
+
+//         console.log("added new row")
+//     }
+   
+    
+// })
+
+
+
+
+// })
+
+
+
+
