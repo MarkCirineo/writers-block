4a027291034beefa1f74530f4f63b806ea2eaa7a
lamperouge <philbohn1791@gmail.com>
2021-10-21 10:39:25 -0400

    get routes done in wcroutes.js

--- controllers/api/wc-routes.js
+++ controllers/api/wc-routes.js
@@ -3,9 +3,42 @@
 const router = require("express").Router();
 const { User, Project, Topic_Sentence, Works_Cited } = require("../../models");
 
-// TODO: GET all Works Cited
+// GET all Works Cited
+router.get("/", async (req, res) => {
+  try {
+    const wcData = await Works_Cited.findAll();
+    res.status(200).json(wcData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-// TODO: GET Works Cited by ID
+// GET Works Cited by ID
+router.get("/:id", async (req, res) => {
+  try {
+    const wcData = await Works_Cited.findOne({
+      where: {
+        id: req.params.id,
+      },
+      include: [
+        {
+          model: User,
+          attributes: ["id", "username"],
+        },
+        {
+          model: Project,
+          attributes: ["title", "thesis"],
+        },
+      ],
+    });
+    if (!wcData) {
+      res.status(404).json({ message: "No Work Cited with this ID" });
+    }
+    res.status(200).json(wcData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // TODO: CREATE a new Works Cited
 
