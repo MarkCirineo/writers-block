4d2b2f228d3a04a7668d5a9324c451abef315246
isaac long <isaaclong123@outlook.com>
2021-10-20 20:19:23 -0400

    set up project page

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,35 +1,9 @@
-// $(document).ready(function(){
-
-
-// var projects = $("#projectGrid").children("div")
-
-// var counts = projects.length;
-
-// const newRow = `</div> <div class="row">`
-
-// const firstRow = `<div class="row">`
-// projects.each(function(){
-   
-//     var id= $(this).attr("id") 
-//     if(id == 1){
-//         $(this).before(firstRow)
-//         console.log(firstRow)
-//     }
-
-//     if( id % 3 === 0 ){
-//         $(this).after(newRow)
-
-//         console.log("added new row")
-//     }
-   
-    
-// })
 
 
 
+$("#newProject").click(function(){
+document.location.replace("/project")
 
-// })
-
-
+})
 
 
