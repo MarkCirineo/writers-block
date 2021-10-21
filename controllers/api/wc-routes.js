c15864b4fe158a263072c0b4bdd288d93c009cfe
lamperouge <philbohn1791@gmail.com>
2021-10-21 10:08:54 -0400

    removed todos from project-routes. Added todos to ts-routes and wc-routes.js

--- controllers/api/wc-routes.js
+++ controllers/api/wc-routes.js
@@ -3,4 +3,14 @@
 const router = require("express").Router();
 const { User, Project, Topic_Sentence, Works_Cited } = require("../../models");
 
+// TODO: GET all Works Cited
+
+// TODO: GET Works Cited by ID
+
+// TODO: CREATE a new Works Cited
+
+// TODO: UPDATE a Works Cited
+
+// TODO: DELETE a Works Cited
+
 module.exports = router;
