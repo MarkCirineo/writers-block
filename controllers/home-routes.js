cf1a20125438d9d3953d69db08e09960da836581
isaac long <isaaclong123@outlook.com>
2021-10-20 22:15:07 -0400

    put in route for dashboard temporarily and finshed homepage.handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,4 +1,26 @@
+const { Project } = require("../models");
+
 // Think of this as views - handling rendering Handlebars
 const router = require("express").Router();
+
+
+router.get('/', async (req, res) => {
+    try {
+      // Get all projects and JOIN with user data
+      const projectData = await Project.findAll();
+  
+      // Serialize data so the template can read it
+      const projects = projectData.map((project) => project.get({ plain: true }));
+  
+      // Pass serialized data and session flag into template
+      res.render('dashboard', { 
+        projects
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+
 
 module.exports = router;
--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -7,3 +7,22 @@ document.location.replace("/project")
 })
 
 
+
+$(function(){
+    var gridheight = $("#newProject").width()* 1.4142;
+    $(".project").css("height", gridheight)
+    console.log(gridheight)
+
+    var h3Margin = gridheight/4;
+
+    $(".project > h3").css("margin-top", h3Margin)
+
+
+    console.log($(".project > h3").length)
+
+
+
+
+})
+
+
