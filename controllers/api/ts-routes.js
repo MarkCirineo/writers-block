a4a356a9c668b7d203def4ddb07b6f1899b8966f
lamperouge <philbohn1791@gmail.com>
2021-10-21 10:08:09 -0400

    removed todos from project-routes. Added todos to ts-routes and wc-routes.js

--- controllers/api/ts-routes.js
+++ controllers/api/ts-routes.js
@@ -3,4 +3,15 @@
 const router = require("express").Router();
 const { User, Project, Topic_Sentence, Works_Cited } = require("../../models");
 
+// TODO: GET all Topic Sentences
+
+// TODO: GET Topic Sentence by ID
+
+// TODO: CREATE a new Topic Sentence
+ 
+// TODO: UPDATE a Topic Sentence
+
+// TODO: DELETE a Topic Sentence
+
+
 module.exports = router;
