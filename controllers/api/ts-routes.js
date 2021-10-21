f05965b66beab6885d1c90f3974445307ceb73cf
lamperouge <philbohn1791@gmail.com>
2021-10-21 10:15:13 -0400

    get routes for ts-routes.js done

--- controllers/api/ts-routes.js
+++ controllers/api/ts-routes.js
@@ -4,14 +4,46 @@ const router = require("express").Router();
 const { User, Project, Topic_Sentence, Works_Cited } = require("../../models");
 
 // TODO: GET all Topic Sentences
+router.get("/", async (req, res) => {
+  try {
+    const tsData = await Topic_Sentence.findAll();
+    res.status(200).json(tsData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // TODO: GET Topic Sentence by ID
+router.get("/:id", async (req, res) => {
+  try {
+    const tsData = await Topic_Sentence.findOne({
+      where: {
+        id: req.params.id,
+      },
+      include: [
+        {
+          model: User,
+          attributes: ["id", "username"],
+        },
+        {
+          model: Project,
+          attributes: ["title", "thesis"],
+        },
+      ],
+    });
+    if (!tsData) {
+      res.status(404).json({ message: "No Topic Sentence with this ID" });
+    }
+    res.status(200).json(tsData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // TODO: CREATE a new Topic Sentence
- 
+
 // TODO: UPDATE a Topic Sentence
 
 // TODO: DELETE a Topic Sentence
-
 
 module.exports = router;
