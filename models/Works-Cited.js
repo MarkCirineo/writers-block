896cd1475d2a15c1fe0adc6dea9d520a2687e9b4
lamperouge <philbohn1791@gmail.com>
2021-10-23 13:48:56 -0400

    changed up WorksCited.js in models to accomodate information needed to properly format citations

--- models/Works-Cited.js
+++ models/Works-Cited.js
@@ -4,32 +4,50 @@ const sequelize = require("../config/connection");
 class Works_Cited extends Model {}
 
 Works_Cited.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        content: {
-            type: DataTypes.TEXT,
-            allowNull: false,
-        },
-        project_id: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: "project",
-                key: "id",
-            },
-        },
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        sequelize,
-        timestamps: false,
-        freezeTableName: true,
-        underscored: true,
-        modelName: "works_cited",
-    }
+    author_first: {
+      type: DataTypes.TEXT,
+    },
+    author_last: {
+      type: DataTypes.TEXT,
+    },
+    title: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    ver_ed: {
+      type: DataTypes.TEXT,
+    },
+    publisher: {
+      type: DataTypes.TEXT,
+    },
+    publication_date: {
+      type: DataTypes.STRING,
+    },
+    location: {
+      type: DataTypes.STRING,
+    },
+    project_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "project",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "works_cited",
+  }
 );
 
 module.exports = Works_Cited;
